7de84cb69e30bcdd0a8b18949595728533d2ef5d
hefju <hefju@live.cn>
2014-06-25 22:50:58 +0800

    被github windows的Revert 和 roll back 搞死了
    Revert 和 roll back  都是直接删除文件和新增文件的,  没有确认.
    结果我有东西没有签入, 使用了这两个功能就永远丢失了.
    没事不要乱按啊.

---
+++ src/models/models.go
@@ -0,0 +1,44 @@
+package models
+import (
+	"strconv"
+	"bytes"
+)
+
+type User struct {
+	Id int64
+	Name string
+}
+
+type Task struct {
+	Id int64
+	Title string
+	Content string
+}
+func(t *Task)String() string {
+	return strconv.FormatInt(t.Id,10)+"\t"+ t.Title+",\t"+t.Content+"\n"
+}
+
+type  TaskList struct {
+	List []Task
+}
+func (t *TaskList)InitTaskList(){
+	t0:= Task{Id :1,Title:"t0",Content:"silce的用法"}
+	t1:= Task{Id :2,Title:"t1",Content:"struct的用法"}
+	t2:=Task{Id:3,Title:"t2",Content:"类型转换(int64 to string)"}
+	t3:=Task{Id:4,Title:"t3",Content:"字符串拼接(stringbuilder)"}
+	t.List=append(t.List,t0,t1,t2,t3)
+}
+func (t *TaskList)String() string{
+	if len(t.List)>0 {
+		br:=bytes.NewBuffer(nil)
+		br.WriteString("id"+"\t"+ "Title"+"\t"+"Content\n")
+		for _,i:=range t.List{
+			br.WriteString(strconv.FormatInt(i.Id,10)+"\t"+ i.Title+",\t"+i.Content+"\n")
+	}
+		return br.String()
+	}else{
+		return "没有内容"
+	}
+}
+
+
